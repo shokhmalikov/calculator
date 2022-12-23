8a5022715ecbbcd82dd431e529f3cbf4197dbcec
John Doe <johndoe@example.com>
2022-12-23 20:06:50 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:calculator/calculator/calculator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) => MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Calculator',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              home: const CalculatorPage(),
+            ));
+  }
+}
